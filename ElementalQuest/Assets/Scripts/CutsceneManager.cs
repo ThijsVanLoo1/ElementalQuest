b01845ccb01c530760c478fa64a049744e288211
Semmetje <semvandenende@gmail.com>
2025-06-27 11:17:05 +0200

    Updated some scripts with comments

--- ElementalQuest/Assets/Scripts/CutsceneManager.cs
+++ ElementalQuest/Assets/Scripts/CutsceneManager.cs
@@ -19,6 +19,7 @@ public class CutsceneManager : MonoBehaviour
 
     private void Start()
     {
+        // Display the first line of text and play sound if present
         lineNumber = 0;
         subtitle.text = lines[0].ToString();
         sources[0].Play();
@@ -26,19 +27,24 @@ public class CutsceneManager : MonoBehaviour
 
     public void NextLine()
     {
+        // Display the next line in the array of lines and sound if present
         lineNumber++;
         subtitle.text = lines[lineNumber].ToString();
         sources[lineNumber].Play();
     }
 
+    // BOSS ONLY - Start QTE
     public void StartQTE()
     {
+        // Trick the QTE script into thinking The French is an ore so QTE script can be reused for this
         QuicktimeEvent.ore = theFrench;
         qteCanvas.SetActive(true);
 
+        // Run Frenchman attacking sequence
         frenchAnimator.SetTrigger("Attack");
     }
 
+    // BOSS ONLY - You defeated the French! GG
     public void SendToMain()
     {
         SceneManager.LoadScene(0);
--- ElementalQuest/Assets/Scripts/TheFrench.cs
+++ ElementalQuest/Assets/Scripts/TheFrench.cs
@@ -7,11 +7,14 @@ public class TheFrench : MonoBehaviour
     public Slider healthbar;
     public AudioSource hit;
     
+    // Attack player function
     public void AttackPlayer()
     {
+        // Decrease health by one
         healthbar.value--;
         hit.Play();
 
+        // If no health left, reload scene
         if(healthbar.value < 1)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
